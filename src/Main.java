93aad738fa8da41a0ea5926314908a2044373b93
stephany <stephany.cornejo00@gmail.com>
2026-02-14 19:21:33 -0600

    Cree el esqueleto del programa

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,54 @@
 import java.util.*;
 
 public class Main {
+
+    private static HashMap<String, String> estudiantes = new HashMap<>();
+    private static Scanner scanner = new Scanner((System.in));
+
     public static void main(String[] args) {
-        // Prueba subiendo a github
+        while(true) {
+            mostraMenu();
+
+            String opcion = scanner.nextLine();
+
+            switch (opcion) {
+                case "1":
+                    agregarAlumno();
+                    break;
+                case "2":
+                    buscarAlumno();
+                    break;
+                case "3":
+                    eliminarAlumno();
+                    break;
+                case "4":
+                    mostrarTodos();
+                    break;
+                case "5":
+                    System.exit(0);
+                default:
+                    System.out.println("Opción invalida. Intente de nuevo");
+            }
+        }
+    }
+
+    private static void mostraMenu() {
+
+    }
+
+    private static void agregarAlumno() {
+
+    }
+
+    private  static void buscarAlumno() {
+
+    }
+
+    private static void eliminarAlumno() {
+
+    }
+
+    private static void mostrarTodos() {
+
     }
 }
